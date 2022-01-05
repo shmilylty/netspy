d37e9350beb715a3e5f7e0a547b621ef43fb2315
Jing Ling <shmilylty@gmail.com>
2022-01-05 16:36:36 +0800

    去掉无用注释

--- cmd/netspy/main.go
+++ cmd/netspy/main.go
@@ -6,17 +6,4 @@ import (
 
 func main() {
 	core.Execute()
-	//log.Info("Hello!")
-	//pinger, _ := ping.NewPinger("192.168.91.134")
-	//pinger.Count =3
-	//_ = pinger.Run()
-	//stats := pinger.Statistics()
-	//fmt.Printf("PING %s (%s):\n", pinger.Addr(), pinger.IPAddr())
-	//fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
-	//fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
-	//	stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
-	//fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
-	//	stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
-	//println(stats.PacketsSent)
-	//println()
 }
